61a6d39b02a212e7f3547dd278114292137b5388
Sakshi Shukla <sakshishukla1008@gmail.com>
2026-01-16 01:21:16 +0530

    commit

--- backend/hls/hls_stream.py
+++ backend/hls/hls_stream.py
@@ -8,17 +8,22 @@ def start_hls(rtsp_url):
     os.makedirs(HLS_DIR, exist_ok=True)
 
     command = [
-        "ffmpeg",
-        "-i", rtsp_url,
-        "-c:v", "libx264",
-        "-preset", "veryfast",
-        "-g", "50",
-        "-sc_threshold", "0",
-        "-hls_time", "2",
-        "-hls_list_size", "5",
-        "-hls_flags", "delete_segments",
-        os.path.join(HLS_DIR, "stream.m3u8")
-    ]
+    "ffmpeg",
+    "-fflags", "nobuffer",
+    "-flags", "low_delay",
+    "-i", rtsp_url,
+    "-c:v", "libx264",
+    "-preset", "ultrafast",
+    "-tune", "zerolatency",
+    "-g", "25",
+    "-sc_threshold", "0",
+    "-hls_time", "1",
+    "-hls_list_size", "3",
+    "-hls_flags", "delete_segments+append_list",
+    "-hls_allow_cache", "0",
+    os.path.join(HLS_DIR, "stream.m3u8")
+]
+
 
     print("Starting FFmpeg HLS stream...")
     subprocess.Popen(command)
